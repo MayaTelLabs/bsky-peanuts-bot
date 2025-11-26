1cfbe2732c1663eb3c905fac6af69fffc2aefabc
Maya Ventura <maya@Hillcrest.local>
2025-11-26 01:53:05 -0500

    switch to randomized image processing

--- src/images/index.ts
+++ src/images/index.ts
@@ -2,50 +2,31 @@ import * as fs from 'fs';
 import * as path from 'path';
 import * as util from 'util';
 
-type GetNextImageOptions = {
-  lastImageName: string | undefined;
-};
-
-type NextImage = {
+type RandomImage = {
   imageName: string;
   absolutePath: string;
-  loopedAround: boolean;
 };
-
-async function getNextImage(options?: GetNextImageOptions): Promise<NextImage> {
-  const { lastImageName } = options || {};
+async function getRandomImage(): Promise<RandomImage> {
   const readdir = util.promisify(fs.readdir);
-  const imagesDir = path.resolve(__dirname, '../../imagequeue');
-  const imageFiles = (await readdir(imagesDir)).sort();
+  const imagesDir = path.resolve(__dirname, '../../imagequeue'); 
+  let imageFiles: string[];
+  try {
+    imageFiles = await readdir(imagesDir);
+  } catch (error) {
+    throw new Error(`Failed to read directory ${imagesDir}: ${error.message}`);
+  }
   const imageRegex = /\.(jpg|jpeg|png|gif|bmp)$/i;
   const validImageFiles = imageFiles.filter((filename) => imageRegex.test(filename));
 
   if (validImageFiles.length === 0) {
     throw new Error('No image files found in the directory.');
   }
-
-  let nextImageIndex = 0; // default value if no lastImageName provided, or it's not found
-  let loopedAround = false;
-
-  if (lastImageName) {
-    const lastImageIndex = validImageFiles.indexOf(lastImageName);
-    if (lastImageIndex >= 0) {
-      nextImageIndex = lastImageIndex + 1;
-      if (nextImageIndex >= validImageFiles.length) {
-        nextImageIndex = 0; // loop back to the first image if we've reached the end
-        loopedAround = true;
-      }
-    }
-  }
-
-  const imageName = validImageFiles[nextImageIndex];
+  const randomIndex = Math.floor(Math.random() * validImageFiles.length);
+  const imageName = validImageFiles[randomIndex];
   const absolutePath = path.join(imagesDir, imageName);
-
   return {
     imageName,
     absolutePath,
-    loopedAround,
   };
 }
-
-export { getNextImage };
+export { getRandomImage };
